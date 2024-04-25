d1e54a31a374166487daab05ea44e8d78a9aeb22
trungluong0806 <>
2024-04-25 13:50:55 +0700

    Project Creation

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author ${LuongThanhTrung}
+ * @version ${}
+ * @created 25/04/2024 13:50
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
